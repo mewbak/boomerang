ce14a85ab5d0873fcb5375414bb5ffcfbc59c548
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-05-01 11:58:36 +0000

    Spelling of 'propagate'
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@294 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/include/boomerang.h
+++ boomerang/include/boomerang.h
@@ -12,7 +12,7 @@
  * OVERVIEW:    interface for the boomerang singleton object
  *============================================================================*/
 /*
- * $Revision: 1.9 $
+ * $Revision: 1.10 $
  * 04 Dec 2002: Trent: Created
  */
 
@@ -59,7 +59,7 @@ public:
     bool noDecompileUp;
     bool traceDecoder;
     const char *dotFile;
-    int numToPropogate;
+    int numToPropagate;
     bool noPromote;
 };
 
