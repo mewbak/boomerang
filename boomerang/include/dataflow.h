4ff5001a983a168322465757de44a35b72ed9c8a
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-06-03 10:55:08 +0000

    Propagate into arguments
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@345 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/include/dataflow.h
+++ boomerang/include/dataflow.h
@@ -13,7 +13,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.28 $
+ * $Revision: 1.29 $
  * 25 Nov 02 - Trent: appropriated for use by new dataflow.
  * 3 July 02 - Trent: created.
  * 03 Feb 03 - Mike: cached dataflow (uses and usedBy)
@@ -285,6 +285,10 @@ public:
     // To/from SSA form
     virtual void   toSSAform(StatementSet& reachin, int memDepth) = 0;
     virtual void fromSSAform(igraph& igm) = 0;
+
+    // Propagate to this statement
+    void propagateTo(int memDepth);
+
 protected:
     virtual void doReplaceRef(Exp* from, Exp* to) = 0;
     bool calcMayAlias(Exp *e1, Exp *e2, int size);
--- boomerang/include/prog.h
+++ boomerang/include/prog.h
@@ -14,7 +14,7 @@
  * OVERVIEW:    interface for the program object.
  *============================================================================*/
 /*
- * $Revision: 1.24 $
+ * $Revision: 1.25 $
  * Created by Mike
  * 24 Mar 98 - Cristina
  *  Changed m_procs to be a list of procedure objects rather than pointers
@@ -167,6 +167,7 @@ public:
 
     // Do decompilation
     void decompile();
+void decompile1();
 
     // All that used to be done in UserProc::decompile, but now done globally.
     // propagation, recalc DFA, remove null and unused statements, compressCfg,
@@ -174,7 +175,14 @@ public:
     void decompileProcs();
 
     // Remove null and unused statements
-    void removeNullUnusedStmts();
+    void removeNullStmts();
+    void removeUnusedStmts();
+
+    // Process constants
+    void processConstants();
+
+    // Repair dataflow... a hack
+    void repairDataflow();
 
     // Generate dotty file
     void generateDotFile();
