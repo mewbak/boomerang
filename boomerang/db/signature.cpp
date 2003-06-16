81110890f7069296767878ffc6f02d6f87638691
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-06-16 00:35:15 +0000

    Note: m[esp + -8] is simplified to m[esp - 8] now
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@365 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/db/signature.cpp
+++ boomerang/db/signature.cpp
@@ -13,7 +13,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.35 $
+ * $Revision: 1.36 $
  * 
  * 15 Jul 02 - Trent: Created.
  * 18 Jul 02 - Mike: Changed addParameter's last param to deflt to "", not NULL
@@ -269,9 +269,9 @@ Signature *CallingConvention::Win32Signature::promote(UserProc *p)
     return this;
 }
 
-Exp *CallingConvention::Win32Signature::getStackWildcard()
-{
-    return new Unary(opMemOf, new Binary(opPlus, new Unary(opRegOf, 
+Exp *CallingConvention::Win32Signature::getStackWildcard() {
+    // Note: m[esp + -8] is simnplified to m[esp - 8] now
+    return new Unary(opMemOf, new Binary(opMinus, new Unary(opRegOf, 
                new Const(28)), new Terminal(opWild)));
 }
 
@@ -445,9 +445,9 @@ Signature *CallingConvention::StdC::PentiumSignature::promote(UserProc *p)
     return this;
 }
 
-Exp *CallingConvention::StdC::PentiumSignature::getStackWildcard()
-{
-    return new Unary(opMemOf, new Binary(opPlus, new Unary(opRegOf, 
+Exp *CallingConvention::StdC::PentiumSignature::getStackWildcard() {
+    // Note: m[esp + -8] is simplified to m[esp - 8] now
+    return new Unary(opMemOf, new Binary(opMinus, new Unary(opRegOf, 
                new Const(28)), new Terminal(opWild)));
 }
 
