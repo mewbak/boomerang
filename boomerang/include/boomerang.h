a1ed1853b95d738f0a76f9ee81a6942a638b8a6f
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-06-19 04:10:16 +0000

    Separated -rb (recursion bust) from -npm (no propagation of multiple expressions all the same)
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@377 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/include/boomerang.h
+++ boomerang/include/boomerang.h
@@ -12,7 +12,7 @@
  * OVERVIEW:    interface for the boomerang singleton object
  *============================================================================*/
 /*
- * $Revision: 1.15 $
+ * $Revision: 1.16 $
  * 04 Dec 2002: Trent: Created
  */
 
@@ -66,6 +66,7 @@ public:
     bool debugDataflow;
     bool debugPrintReach;
     bool debugPrintSSA;
+    bool noPropMult;
 };
 
 #define VERBOSE Boomerang::get()->vFlag
