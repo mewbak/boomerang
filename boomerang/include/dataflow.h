f2a4f2afb6a868656c411e375868386fd37ab283
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-07-23 08:54:35 +0000

    Added StatementSet::operator<
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@388 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/include/dataflow.h
+++ boomerang/include/dataflow.h
@@ -13,7 +13,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.36 $
+ * $Revision: 1.37 $
  * 25 Nov 02 - Trent: appropriated for use by new dataflow.
  * 3 July 02 - Trent: created.
  * 03 Feb 03 - Mike: cached dataflow (uses and usedBy)
@@ -69,8 +69,9 @@ public:
     bool defines(Exp* loc);                 // Search; returns true if any
                                             // statement defines loc
     void clear() {sset.clear();}            // Clear the set
-    bool operator==(const StatementSet& o) const // Compare
+    bool operator==(const StatementSet& o) const    // Compare if equal
         { return sset == o.sset;}
+    bool operator<(const StatementSet& o) const;    // Compare if less
     void prints();                          // Print to std::cerr (for debug)
     void printNums(std::ostream& os);       // Print statements as numbers
 };
