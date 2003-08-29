ecbe172ff567a85cce03830625da867059b809b4
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-08-29 09:13:45 +0000

    class NJMCDecoder under GC now
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@544 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/include/decoder.h
+++ boomerang/include/decoder.h
@@ -15,7 +15,7 @@
  *============================================================================*/
 
 /* 
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  * 08 Apr 02 - Mike: Mods for boomerang
  */
 
@@ -25,6 +25,7 @@
 #include <list>
 #include "types.h"
 #include "rtl.h"
+#include "gc_cpp.h"
 
 class Exp;
 class RTL;
@@ -96,7 +97,7 @@ struct DecodeResult {
  * The NJMCDecoder class is a class that contains NJMC generated decoding
  * methods.
  *============================================================================*/
-class NJMCDecoder {
+class NJMCDecoder : public gc {
 public:
     /*
      * Constructor and destructor
