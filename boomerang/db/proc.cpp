5bc9972105be5ebe2464dad6373ecd19edf77089
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-09-03 01:23:27 +0000

    Added a call to clone in addReturn
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@569 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/db/proc.cpp
+++ boomerang/db/proc.cpp
@@ -20,7 +20,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.126 $
+ * $Revision: 1.127 $
  *
  * 14 Mar 02 - Mike: Fixed a problem caused with 16-bit pushes in richards2
  * 20 Apr 02 - Mike: Mods for boomerang
@@ -1441,7 +1441,10 @@ void UserProc::addReturn(Exp *e)
     }
     for (std::vector<ReturnStatement*>::iterator it = returnStatements.begin();
          it != returnStatements.end(); it++)
-            (*it)->addReturn(e1);
+            // Each returnStatement needs a separate copy of the return
+            // expression, otherwise there will be problems when one is
+            // changed (e.g. substituted into) but another is not (yet)
+            (*it)->addReturn(e1->clone());
     Proc::addReturn(e);
 }
 
