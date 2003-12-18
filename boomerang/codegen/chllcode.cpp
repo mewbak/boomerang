646c3946c18283f597fedddbd1c4efbc132af7a8
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2003-12-18 03:57:25 +0000

    Commented out a dodgy assert (Trent to check)
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@696 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/codegen/chllcode.cpp
+++ boomerang/codegen/chllcode.cpp
@@ -16,7 +16,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.46 $
+ * $Revision: 1.47 $
  * 20 Jun 02 - Trent: Quick and dirty implementation for debugging
  * 28 Jun 02 - Trent: Starting to look better
  * 22 May 03 - Mike: delete -> free() to keep valgrind happy
@@ -496,7 +496,9 @@ void CHLLCode::appendExp(char *str, Exp *exp)
                     strcat(str, "/* type failure */ ");
                     break;
                 }
-                assert(ty->resolvesToCompound());
+                // Trent: what were you thinking here? Fails for things like
+                // local11.lhHeight (where local11 is a register)
+                //assert(ty->resolvesToCompound());
                 if (b->getSubExp1()->getOper() == opMemOf) {
                     appendExp(str, b->getSubExp1()->getSubExp1());
                     strcat(str, "->");
