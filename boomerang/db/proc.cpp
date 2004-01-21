3629156ef01001b63987f33451adeecc8f015e3e
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2004-01-21 07:02:02 +0000

    Type analysis affects locals as well as globals now
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@750 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/db/proc.cpp
+++ boomerang/db/proc.cpp
@@ -20,7 +20,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.162 $
+ * $Revision: 1.163 $
  *
  * 14 Mar 02 - Mike: Fixed a problem caused with 16-bit pushes in richards2
  * 20 Apr 02 - Mike: Mods for boomerang
@@ -3160,14 +3160,17 @@ void UserProc::typeAnalysis(Prog* prog) {
             Exp* loc = ((Unary*)cc->first)->getSubExp1();
             assert(cc->second->isTypeVal());
             Type* ty = ((TypeVal*)cc->second)->getType();
-            if (loc->isSubscript() && (loc = ((RefExp*)loc)->getSubExp1(),
-                  loc->isGlobal())) {
+            if (loc->isSubscript())
+                loc = ((RefExp*)loc)->getSubExp1();
+            if (loc->isGlobal()) {
                 char* nam = ((Const*)((Unary*)loc)->getSubExp1())->getStr();
                 prog->setGlobalType(nam, ty->clone());
+            } else if (loc->isLocal()) {
+                char* nam = ((Const*)((Unary*)loc)->getSubExp1())->getStr();
+                setLocalType(nam, ty);
             }
         }
     }
-
 }
 
 bool UserProc::searchAndReplace(Exp *search, Exp *replace)
