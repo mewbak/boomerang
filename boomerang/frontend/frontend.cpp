414d2e3deb0abc8fedf6a1a1d5bc94c29566d09b
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2004-05-31 05:19:43 +0000

    Changes for using helperFunc properly (Pentium)
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@881 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/frontend/frontend.cpp
+++ boomerang/frontend/frontend.cpp
@@ -10,14 +10,14 @@
  */
 
 /*==============================================================================
- * FILE:       frontend.cc
+ * FILE:       frontend.cpp
  * OVERVIEW:   This file contains common code for all front ends. The majority
  *              of frontend logic remains in the source dependent files such as
- *              frontsparc.cc
+ *              frontsparc.cpp
  *============================================================================*/
 
 /*
- * $Revision: 1.58 $
+ * $Revision: 1.59 $
  * 08 Apr 02 - Mike: Mods to adapt UQBT code to boomerang
  * 16 May 02 - Mike: Moved getMainEntry point here from prog
  * 09 Jul 02 - Mike: Fixed machine check for elf files (was checking endianness
@@ -680,6 +680,7 @@ bool FrontEnd::processProc(ADDRESS uAddr, UserProc* pProc, std::ofstream &os,
                         // will check for machine specific func calls
                         if (helperFunc(uNewAddr, uAddr, BB_rtls)) {
                             // We have already added to BB_rtls
+                            pRtl = NULL;       // Discard the call semantics
                             break;
                         }
 
@@ -802,7 +803,7 @@ bool FrontEnd::processProc(ADDRESS uAddr, UserProc* pProc, std::ofstream &os,
         
                 } // switch (s->getKind())
             }
-            if (BB_rtls)
+            if (BB_rtls && pRtl)
                 // If non null, we haven't put this RTL into a the current BB
                 // as yet
                 BB_rtls->push_back(pRtl);
