87379725ce0f4bb24134acf2e0583d1f5df1bc40
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2004-06-20 13:38:41 +0000

    Added a check for refexp in some switch code
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@915 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/db/basicblock.cpp
+++ boomerang/db/basicblock.cpp
@@ -15,7 +15,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.77 $
+ * $Revision: 1.78 $
  * Dec 97 - created by Mike
  * 18 Apr 02 - Mike: Changes for boomerang
  * 04 Dec 02 - Mike: Added isJmpZ
@@ -1753,7 +1753,8 @@ void findSwParams(char form, Exp* e, Exp*& expr, ADDRESS& T) {
             // Pattern: <base>{}[<index>]{}
             e = ((RefExp*)e)->getSubExp1();
             Exp* base = ((Binary*)e)->getSubExp1();
-            base = ((RefExp*)base)->getSubExp1();
+            if (base->isSubscript())
+                base = ((RefExp*)base)->getSubExp1();
             Exp* con = ((Location*)base)->getSubExp1();
             char* gloName = ((Const*)con)->getStr();
             UserProc* p = ((Location*)base)->getProc();
