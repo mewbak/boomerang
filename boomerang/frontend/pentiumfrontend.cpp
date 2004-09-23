d5f5e951f08173d18487be0e04b183b2bb33993f
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2004-09-23 13:12:49 +0000

    -O works with setcc instructions again
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1125 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/frontend/pentiumfrontend.cpp
+++ boomerang/frontend/pentiumfrontend.cpp
@@ -16,7 +16,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.47 $
+ * $Revision: 1.48 $
  * 21 Oct 98 - Mike: converted from frontsparc.cc
  * 21 May 02 - Mike: Mods for boomerang
  * 27 Nov 02 - Mike: Fixed a bug in the floating point fixup code, which was
@@ -1294,7 +1294,7 @@ void PentiumFrontEnd::processOverlapped(UserProc* proc) {
 	StatementList::iterator it;
 	for (it = stmts.begin(); it != stmts.end(); it++) {
 		Statement* s = *it;
-		if (!s->isAssign()) continue;
+		if (!s->isAssignment()) continue;
 		Exp* lhs = s->getLeft();
 		if (!lhs->isRegOf()) continue;
 		Const* c = (Const*)((Location*)lhs)->getSubExp1();
