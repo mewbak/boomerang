8475b53e24fd4c5af47faa435e9a8dd58970f400
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2004-11-04 21:26:06 +0000

    Use ErrLogger to warn about known unit test failure
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1239 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/db/DfaTest.cpp
+++ boomerang/db/DfaTest.cpp
@@ -4,13 +4,24 @@
  *				tests the data flow based type analysis code
  *============================================================================*/
 /*
- * $Revision: 1.1 $
+ * $Revision: 1.2 $
  *
  * 19 Oct 04 - Mike: Created
  */
 
 #include "DfaTest.h"
+#include <iostream>		// For std::cerr
+#include <log.h>
+#include <boomerang.h>
 
+class ErrLogger : public Log {
+public:
+	virtual Log &operator<<(const char *str) {
+	 std::cerr << str;
+		return *this;
+	}
+	virtual ~ErrLogger() {};
+};
 /*==============================================================================
  * FUNCTION:		DfaTest::registerTests
  * OVERVIEW:		Register the test functions in the given suite
@@ -164,6 +175,9 @@ void DfaTest::testMeetSize () {
 	ch = false;
 	res = s32.meetWith(&s16, ch);
 	CPPUNIT_ASSERT(ch == false);
+
+	// There is a known failure here; to show the warning, use ErrLogger
+	Boomerang::get()->setLogger(new ErrLogger);
 
 	res = s16.meetWith(&flt, ch);
 	CPPUNIT_ASSERT(ch == true);
