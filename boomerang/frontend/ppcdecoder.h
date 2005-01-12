a0f65ebc6825c038ea1d9905ab85e6fcc45e6133
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2005-01-12 22:47:49 +0000

    Oops - removed the Sparc r0 is always 0 thing
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1354 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/frontend/ppcdecoder.h
+++ boomerang/frontend/ppcdecoder.h
@@ -14,7 +14,7 @@
  *============================================================================*/
 
 /* 
- * $Revision: 1.1 $
+ * $Revision: 1.2 $
  * 23 Nov 04 - Jay Sweeney and Alejandro Dubrovsky: Created.
  */
 
@@ -52,8 +52,7 @@ private:
 	 */
 	Exp*	dis_Eaddr(ADDRESS pc, int size = 0);
 	Exp*	dis_RegImm(ADDRESS pc);
-	Exp*	dis_RegLhs(unsigned r);
-	Exp*	dis_RegRhs(unsigned r);
+	Exp*	dis_Reg(unsigned r);
 
 	void	unused(int x);
 	RTL*	createBranchRtl(ADDRESS pc, std::list<Statement*>* stmts,
