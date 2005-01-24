3120a8614e24edd9782480edcd7a4ed650ad11f5
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2005-01-24 05:02:50 +0000

    Fixed semantics of MTSPR
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1393 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/frontend/machine/ppc/decoder.m
+++ boomerang/frontend/machine/ppc/decoder.m
@@ -12,7 +12,7 @@
  * OVERVIEW:   Implementation of the PPC specific parts of the PPCDecoder class.
  *============================================================================*/
 
-/* $Revision: 1.16 $
+/* $Revision: 1.17 $
  *
  * 23/Nov/04 - Jay Sweeney and Alajandro Dubrovsky: Created
  **/
@@ -161,14 +161,15 @@ DecodeResult& PPCDecoder::decodeInstruction (ADDRESS pc, int delta) {
 	| mfspr (rd, uimm) [name] =>
 		stmts = instantiate(pc, name, DIS_RD, DIS_UIMM);
 	| mtspr (uimm, rs) [name] =>
-		if ((uimm >> 5) &1) {		// FIXME: Fix shift amounts
-		  if ((uimm >> 8) & 1) {
-			stmts = instantiate(pc, "MTCTR" , DIS_RS);
-		  } else {
-			stmts = instantiate(pc, "MTXER" , DIS_RS);
-		  }
-		} else {
-			stmts = instantiate(pc, "MTLR" , DIS_RS);
+		switch (uimm) {
+			case 1:
+				stmts = instantiate(pc, "MTXER" , DIS_RS); break;
+			case 8:
+				stmts = instantiate(pc, "MTLR" , DIS_RS); break;
+			case 9:
+				stmts = instantiate(pc, "MTCTR" , DIS_RS); break;
+			default:
+				std::cerr << "ERROR: MTSPR instruction with invalid S field: " << uimm << "\n";
 		}
 		::unused(name);
 	| bl (reladdr) [name] =>
@@ -296,8 +297,8 @@ DecodeResult& PPCDecoder::decodeInstruction (ADDRESS pc, int delta) {
 		std::cerr << "HACK " << name << "\n";
 //		PPC_COND_JUMP(name, 4, new Unary(opMachFtr, new Const("%CTR")), (BRANCH_TYPE)0, BIcr);
 
-	| balctr(BIcr) =>
-		computedJump("balctr", 4, new Unary(opMachFtr, new Const("%CTR")), pc, stmts, result);
+	| balctr(BIcr) [name] =>
+		computedJump(name, 4, new Unary(opMachFtr, new Const("%CTR")), pc, stmts, result);
 		
 	// bcc_ is blt | ble | beq | bge | bgt | bnl | bne | bng | bso | bns | bun | bnu | bal
 	| bltlr(BIcr) [name] =>
