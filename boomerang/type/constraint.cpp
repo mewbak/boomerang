bd0c559deec798b98fd7c2e916b024a0db83cb22
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2005-08-14 02:17:20 +0000

    Minor mods for OS X
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1646 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/type/constraint.cpp
+++ boomerang/type/constraint.cpp
@@ -13,7 +13,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.17 $	// 1.16.6.1
+ * $Revision: 1.18 $	// 1.16.6.1
  *
  * 22 Aug 03 - Mike: Created
  */
@@ -338,9 +338,10 @@ std::ostringstream os; conSet.print(os); LOG << os.str().c_str();
 		}
 	}
 
-{LOG << "\n" << disjunctions.size() << " disjunctions: "; std::list<Exp*>::iterator dd; for (dd = disjunctions.begin(); dd != disjunctions.end(); dd++) LOG << *dd << ",\n"; LOG << "\n";}
-LOG << fixed.size() << " fixed: " << fixed.prints();
-LOG << equates.size() << " equates: " << equates.prints();
+	{LOG << "\n" << (unsigned)disjunctions.size() << " disjunctions: "; std::list<Exp*>::iterator dd;
+	 for (dd = disjunctions.begin(); dd != disjunctions.end(); dd++) LOG << *dd << ",\n"; LOG << "\n";}
+	LOG << fixed.size() << " fixed: " << fixed.prints();
+	LOG << equates.size() << " equates: " << equates.prints();
 
 	// Substitute the fixed types into the disjunctions
 	substIntoDisjuncts(fixed);
