620f870eefe9f2130f80d3af38167a5383714140
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2005-09-13 12:45:46 +0000

    Indirect calls to library functions (e.g. Win32) set theReturnStatement now
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1711 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/frontend/frontend.cpp
+++ boomerang/frontend/frontend.cpp
@@ -17,7 +17,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.99 $	// 1.89.2.7
+ * $Revision: 1.100 $	// 1.89.2.7
  * 08 Apr 02 - Mike: Mods to adapt UQBT code to boomerang
  * 16 May 02 - Mike: Moved getMainEntry point here from prog
  * 09 Jul 02 - Mike: Fixed machine check for elf files (was checking endianness rather than machine type)
@@ -554,7 +554,7 @@ bool FrontEnd::processProc(ADDRESS uAddr, UserProc* pProc, std::ofstream &os, bo
 						BB_rtls = NULL;					// New RTLList for next BB
 						break;							// Just leave it alone
 					}
-					// FIXME: check if this can happen any more
+					// Check for indirect calls to library functions, especially in Win32 programs
 					if (pDest && pDest->getOper() == opMemOf &&
 							pDest->getSubExp1()->getOper() == opIntConst && 
 							pBF->IsDynamicLinkedProcPointer(((Const*)pDest->getSubExp1())->getAddr())) {
@@ -575,8 +575,9 @@ bool FrontEnd::processProc(ADDRESS uAddr, UserProc* pProc, std::ofstream &os, bo
 						std::list<RTL*> *ret_rtls = new std::list<RTL*>();
 						stmt_list = new std::list<Statement*>;
 						stmt_list->push_back(ret);
-						ret_rtls->push_back(new RTL(pRtl->getAddress()+1, stmt_list));
-						PBB pret = pCfg->newBB(ret_rtls, RET, 0);
+						//ret_rtls->push_back(new RTL(pRtl->getAddress()+1, stmt_list));
+						//PBB pret = pCfg->newBB(ret_rtls, RET, 0);
+						PBB pret = createReturnBlock(pProc, ret_rtls, new RTL(pRtl->getAddress()+1, stmt_list));
 						pret->addInEdge(pBB);
 						pBB->setOutEdge(0, pret);
 						sequentialDecode = false;
