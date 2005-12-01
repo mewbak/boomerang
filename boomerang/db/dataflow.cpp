1e6a387f7aa0eb4f1e057ea326d1d6d1605f8965
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2005-12-01 05:23:11 +0000

    Oops - booked in HACK debugging again
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1752 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/db/dataflow.cpp
+++ boomerang/db/dataflow.cpp
@@ -13,7 +13,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.49 $	// 1.43.2.24
+ * $Revision: 1.50 $	// 1.43.2.24
  * 15 Mar 05 - Mike: Separated from cfg.cpp
  */
 
@@ -314,8 +314,6 @@ void DataFlow::renameBlockVars(UserProc* proc, int n, int memDepth, bool clearSt
 				Exp* x = *xx;
 				// Ignore variables of the wrong memory depth
 				if (x->getMemDepth() != memDepth) continue;
-if (S->getNumber() == 64 && memDepth == 2)
- std::cerr << "HACK!\n";
 				Statement* def = NULL;
 				if (x->isSubscript()) {					// Already subscripted?
 					// No renaming required, but redo the usage analysis, in case this is a new return
