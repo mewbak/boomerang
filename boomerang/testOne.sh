4830c086dc7b3bf7558477088c2b77db3021d90a
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2005-12-14 22:51:51 +0000

    Replaced tr with sed for the control character filter
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1777 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/testOne.sh
+++ boomerang/testOne.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# testOne.sh functional test script $Revision: 1.13 $ # 1.10.2.1
+# testOne.sh functional test script $Revision: 1.14 $ # 1.10.2.1
 # Call with test platform, test-program test-set [,option [,arguments]]
 # test-set is a char usually 1-9 for the various .out files, usually use 1 for .out1
 # e.g. "./testOne.sh pentium hello"
@@ -51,7 +51,8 @@ else
 				diff -c test/source/$2.out$3 functest.out > functest.tmp
 				ret=$?
 				# Filter out control chars that may happen due to bad decomp.
-				tr -s -d < functest.tmp [:cntrl:] >> functest.res
+				# tr -s -d < functest.tmp [:cntrl:] >> functest.res
+				sed -e "s/[[:cntrl:]]//g" functest.tmp >> functest.res
 				if [[ ret -ne 0 ]]; then
 					echo Result for $1 $2: FAILED diff set $3 >> functest.res
 				else
