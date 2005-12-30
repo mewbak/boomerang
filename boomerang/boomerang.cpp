23bcd1ef6f321a16c834ea223605bbf09aa69879
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2005-12-30 05:37:25 +0000

    Remove the Memoisable base class for many objects; attempt to save memory\!
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1820 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/boomerang.cpp
+++ boomerang/boomerang.cpp
@@ -6,12 +6,12 @@
  * OVERVIEW:   Command line processing for the Boomerang decompiler
  *============================================================================*/
 /*
- * $Revision: 1.137 $	// 1.115.2.5
+ * $Revision: 1.138 $	// 1.115.2.5
  *
  * 28 Jan 05 - G. Krol: Separated -h output into sections and neatened
 */
 
-#define VERSION "alpha 0.2 28/Dec/2005"
+#define VERSION "alpha 0.2 30/Dec/2005"
 
 #include <iostream>
 #include <fstream>
