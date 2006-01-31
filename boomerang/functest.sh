be39f77dfc8b3d4a8dfd018bf10916d0068a8a51
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2006-01-31 06:25:57 +0000

    Added fibo3, moved up recursion to non-problem area
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1863 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/functest.sh
+++ boomerang/functest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# functest.sh functional test script $Revision: 1.36 $	# 1.23.2.1
+# functest.sh functional test script $Revision: 1.37 $	# 1.23.2.1
 # Note: to test with data flow based type analysis, pass a parameter of -Td
 #
 # 02 Feb 05 - Mike: Conditional tests for no type analysis. So all tests should pass whether -Td is passed or not
@@ -55,6 +55,10 @@ $TESTONE pentium fibo-O4		1 "$BOOMSW" < test/source/fibo-O4.in1
 $TESTONE sparc   fibo-O4		1 "$BOOMSW" < test/source/fibo-O4.in1
 $TESTONE pentium fibo-O4		3 "$BOOMSW" < test/source/fibo-O4.in3
 $TESTONE sparc   fibo-O4		3 "$BOOMSW" < test/source/fibo-O4.in3
+$TESTONE pentium fibo3			3 "$BOOMSW" < test/source/fibo-O4.in3
+$TESTONE sparc   fibo3			3 "$BOOMSW" < test/source/fibo-O4.in3
+$TESTONE pentium recursion		2 "$BOOMSW" 2
+#$TESTONE sparc   recursion		2 "$BOOMSW" 2
 $TESTONE pentium global1		1 "$BOOMSW"
 $TESTONE sparc   global1		1 "$BOOMSW"
 $TESTONE pentium global2		1 "$BOOMSW"
@@ -117,12 +121,9 @@ $TESTONE sparc   global3		1 "$BOOMSW"
 
 echo
 echo === Known faillures ===
-#$TESTONE sparc   recursion		2 "$BOOMSW" 2
 $TESTONE pentium stattest		1 "$BOOMSW"
 $TESTONE sparc   stattest		1 "$BOOMSW"
 $TESTONE sparc	elfhashtest		1 "$BOOMSW"
-# pentium recursion has some weird dataflow issues
-$TESTONE pentium recursion		2 "$BOOMSW" 2
 # Special for Mike
 $TESTONE pentium recursion2		1 "$BOOMSW"
 
