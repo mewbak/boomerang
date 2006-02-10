5f4b6fad627446e6552d12b1ae7f4e8f28e88c95
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2006-02-10 22:11:43 +0000

    Changed the build date
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1879 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/boomerang.cpp
+++ boomerang/boomerang.cpp
@@ -6,12 +6,12 @@
  * OVERVIEW:   Command line processing for the Boomerang decompiler
  *============================================================================*/
 /*
- * $Revision: 1.140 $	// 1.115.2.5
+ * $Revision: 1.141 $	// 1.115.2.5
  *
  * 28 Jan 05 - G. Krol: Separated -h output into sections and neatened
 */
 
-#define VERSION "alpha 0.2 30/Dec/2005"
+#define VERSION "alpha 0.2 10/Feb/2006"
 
 #include <iostream>
 #include <fstream>
