4851f9cfbe6cb0728c8df847caeab63024a1faaa
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2006-04-05 03:24:36 +0000

    Added insertCasts(), setParamType(int, type)
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1961 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/include/proc.h
+++ boomerang/include/proc.h
@@ -16,7 +16,7 @@
  *============================================================================*/
 
 /*
- *	$Revision: 1.137 $	// 1.115.2.27
+ *	$Revision: 1.138 $	// 1.115.2.27
  */
 
 #ifndef _PROC_H_
@@ -497,6 +497,8 @@ virtual				~UserProc();
 		void		recursionGroupAnalysis(ProcList* path, int indent);
 		/// Global type analysis (for this procedure).
 		void		typeAnalysis();
+		/// Inserting casts as needed (for this procedure)
+		void		insertCasts();
 		// Range analysis (for this procedure).
 		void		rangeAnalysis();
 		// Detect and log possible buffer overflows
@@ -708,6 +710,7 @@ public:
 virtual void		renameParam(const char *oldName, const char *newName);
 
 		void		setParamType(const char* nam, Type* ty);
+		void		setParamType(int idx, Type* ty);
 
 		/**
 		 * Print the locals declaration in C style.
