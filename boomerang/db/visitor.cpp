c7e5329f0e4bb6e827f2c095596a51cd44a346f9
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2006-04-08 01:40:46 +0000

    Undid a change of Gerards; changes must have crossed
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@1975 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/db/visitor.cpp
+++ boomerang/db/visitor.cpp
@@ -7,7 +7,7 @@
  *			   classes.
  *============================================================================*/
 /*
- * $Revision: 1.45 $
+ * $Revision: 1.46 $
  *
  * 14 Jun 04 - Mike: Created, from work started by Trent in 2003
  */
@@ -883,14 +883,12 @@ Exp* ExpCastInserter::postVisit(Binary *e) {
 		case opGtrEqUns:
 		case opShiftR: {
 			Type* tl = e->getSubExp1()->ascendType();
-			// Gerard: Changed !isUnsigned to isSigned.
-			// (the CVS version of IntegerType does not contain isUnsigned)
-			if (!tl->isInteger() || tl->asInteger()->isSigned()) {
+			if (!tl->isInteger() || !tl->asInteger()->isUnsigned()) {
 				e->setSubExp1(new TypedExp(new IntegerType(tl->getSize(), -1), e->getSubExp1()));
 			}
 			if (op != opShiftR) {
 				Type* tr = e->getSubExp2()->ascendType();
-				if (!tr->isInteger() || tr->asInteger()->isSigned()) {
+				if (!tr->isInteger() || !tr->asInteger()->isUnsigned()) {
 					e->setSubExp2(new TypedExp(new IntegerType(tr->getSize(), -1), e->getSubExp2()));
 				}
 			}
