a63f4ed3a4e4b21cf41d52b14d067bd8a8920ca6
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2006-07-31 01:32:37 +0000

    Visual C++ 2005 uses "debug" with lower case d for debug directory name; fix debugging hack
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@2129 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/boomerang.cpp
+++ boomerang/boomerang.cpp
@@ -6,7 +6,7 @@
  * OVERVIEW:   Command line processing for the Boomerang decompiler
  *============================================================================*/
 /*
- * $Revision: 1.169 $	// 1.115.2.5
+ * $Revision: 1.170 $	// 1.115.2.5
  *
  * 28 Jan 05 - G. Krol: Separated -h output into sections and neatened
 */
@@ -660,7 +660,9 @@ int Boomerang::commandLine(int argc, const char **argv)
 	}
 #ifdef _MSC_VER						// For the console mode version; Windows GUI will override in windows.cpp
 	// As a special case for MSVC testing, make the program path the parent of the dir with the .exe
-	j = progPath.find("Debug\\", progPath.length() - (5+1));
+	j = progPath.find("ebug\\", progPath.length() - (4+1));
+	if (j != std::string::npos)
+		j--;			// Point to the 'd' or 'D'
 	if (j == std::string::npos) {
 			j = progPath.rfind("elease\\", progPath.length() - (6+1));
 			if (j != std::string::npos)
