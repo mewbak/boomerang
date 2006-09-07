243df92fd071d3e83682ab74e49cff55c0d4a994
emmerik <emmerik@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2006-09-07 07:03:52 +0000

    Updated date/version
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@2207 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/boomerang.cpp
+++ boomerang/boomerang.cpp
@@ -6,13 +6,13 @@
  * OVERVIEW:   Command line processing for the Boomerang decompiler
  *============================================================================*/
 /*
- * $Revision: 1.179 $	// 1.115.2.5
+ * $Revision: 1.180 $	// 1.115.2.5
  *
  * 28 Jan 05 - G. Krol: Separated -h output into sections and neatened
  * 02 Sep 06 - Mike: introduced USE_XML to make it easy to disable use of the expat library
 */
 
-#define VERSION "alpha 0.3 05/Sep/2006"
+#define VERSION "alpha 0.3 07/Sep/2006"
 
 #if __CYGWIN__
 #define USE_XML 0			// Cygwin has a weird problem that causes libBinaryFile.dll not to load if the expat library
