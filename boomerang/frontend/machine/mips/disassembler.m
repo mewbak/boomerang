76c7f963eb7c8d75d57c5300ec94e0ad3ca03f33
thenihilist <thenihilist@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2007-11-19 23:35:43 +0000

    Tidy
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@2241 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/frontend/machine/mips/disassembler.m
+++ boomerang/frontend/machine/mips/disassembler.m
@@ -1,19 +1,19 @@
-dnl /****************************************************************
-dnl *
-dnl * FILENAME
-dnl *
-dnl *   \file disassembler.m
-dnl *
-dnl * PURPOSE 
-dnl *
-dnl * MIPS disassembly.
-dnl *
-dnl * AUTHOR 
-dnl *
-dnl *   \author Markus Gothe, nietzsche@lysator.liu.se
-dnl *
-dnl * REVISION 
-dnl *
-dnl *   $Id: disassembler.m,v 1.1 2007-11-18 16:49:36 thenihilist Exp $
-dnl *
-dnl *****************************************************************/
+/****************************************************************
+ *
+ * FILENAME
+ *
+ *   \file disassembler.m
+ *
+ * PURPOSE 
+ *
+ * MIPS disassembly.
+ *
+ * AUTHOR 
+ *
+ *   \author Markus Gothe, nietzsche@lysator.liu.se
+ *
+ * REVISION 
+ *
+ *   $Id: disassembler.m,v 1.2 2007-11-19 23:35:43 thenihilist Exp $
+ *
+ *****************************************************************/
