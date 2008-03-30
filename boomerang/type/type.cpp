a0749c223ca483647b2ad38261aa146d7692e594
thenihilist <thenihilist@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2008-03-30 01:38:36 +0000

    const_cast<char *>
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@2279 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/type/type.cpp
+++ boomerang/type/type.cpp
@@ -15,7 +15,7 @@
  *============================================================================*/
 
 /*
- * $Revision: 1.70 $	// 1.44.2.1
+ * $Revision: 1.71 $	// 1.44.2.1
  *
  * 28 Apr 02 - Mike: getTempType() returns a Type* now
  * 26 Aug 03 - Mike: Fixed operator< (had to re-introduce an enum... ugh)
@@ -1264,7 +1264,7 @@ bool DataIntervalMap::isClear(ADDRESS addr, unsigned size) {
 // With the forced parameter: are we forcing the name, the type, or always both?
 void DataIntervalMap::addItem(ADDRESS addr, char* name, Type* ty, bool forced /* = false */) {
 	if (name == NULL)
-		name = "<noname>";
+		name = const_cast<char *>("<noname>");
 	DataIntervalEntry* pdie = find(addr);
 	if (pdie == NULL) {
 		// Check that this new item is compatible with any items it overlaps with, and insert it
