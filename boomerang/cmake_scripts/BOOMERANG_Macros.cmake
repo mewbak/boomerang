18e960b2670d9af166599dbaf7444cac79c26535
nemerle <nemerle@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2009-04-03 12:19:19 +0000

    Missing directory. Preliminaries before implementing and integrating CppUnit testing.
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@2359 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

---
+++ boomerang/cmake_scripts/BOOMERANG_Macros.cmake
@@ -0,0 +1,46 @@
+#this macro expects all loader names to be the same as their cpp files
+MACRO(BOOMERANG_ADD_LOADER loader_name)
+	IF(NOT ${loader_name}_LOADER_VISITED)
+		# add the loader as a dll
+		ADD_LIBRARY(${loader_name} SHARED 
+					${loader_name}.cpp 
+					${loader_name}.h
+					SymTab.cpp 
+					SymTab.h
+					${ARGN})
+		# all loaders depend on BinaryFile
+		TARGET_LINK_LIBRARIES(${loader_name} BinaryFile)
+		ADD_DEPENDENCIES(${loader_name} BinaryFile)
+		# add this loader to project's global loader list
+		SET(BOOMERANG_LOADERS ${BOOMERANG_LOADERS} ${loader_name} CACHE INTERNAL "")		
+		SET(${loader_name}_LOADER_VISITED CACHE INTERNAL "")
+	ENDIF(NOT ${loader_name}_LOADER_VISITED)
+ENDMACRO(BOOMERANG_ADD_LOADER)
+
+# this macro will try to add (frontend_name+decoder) and 
+# (frontend_name+frontend) as sources
+MACRO(BOOMERANG_ADD_FRONTEND frontend_name)
+	IF(NOT ${frontend_name}_FRONTEND_VISITED)
+		# add the loader as a static
+		ADD_LIBRARY(Frontend_${frontend_name} STATIC
+		${frontend_name}decoder.cpp
+		${frontend_name}frontend.cpp
+		${frontend_name}decoder.h
+		${frontend_name}frontend.h
+		 ${ARGN}
+		 )
+		# add this generator to project's global loader list
+		SET(BOOMERANG_FRONTENDS ${BOOMERANG_FRONTENDS} Frontend_${frontend_name} CACHE INTERNAL "")		
+		SET(${frontend_name}_FRONTEND_VISITED)
+	ENDIF(NOT ${frontend_name}_FRONTEND_VISITED)
+ENDMACRO(BOOMERANG_ADD_FRONTEND)
+
+MACRO(BOOMERANG_ADD_CODEGEN gen_name)
+	IF(NOT ${gen_name}_CODEGEN_VISITED)
+		# add the loader as a static
+		ADD_LIBRARY(${gen_name} STATIC ${ARGN})
+		# add this generator to project's global loader list
+		SET(BOOMERANG_CODE_GENERATORS ${BOOMERANG_CODE_GENERATORS} ${gen_name} CACHE INTERNAL "")		
+		SET(${gen_name}_CODEGEN_VISITED true CACHE INTERNAL "")
+	ENDIF(NOT ${gen_name}_CODEGEN_VISITED)
+ENDMACRO(BOOMERANG_ADD_CODEGEN)
