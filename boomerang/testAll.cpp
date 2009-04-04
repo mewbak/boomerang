e7ddbc22867c1fddb29782e51326b3052f4c956d
nemerle <nemerle@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2009-04-04 15:14:20 +0000

    Fixed includes for gcc.
    git-svn-id: svn://svn.code.sf.net/p/boomerang/code/trunk@2365 dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5

--- boomerang/testAll.cpp
+++ boomerang/testAll.cpp
@@ -1,9 +1,11 @@
+#include <stdexcept>
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
 #include <cppunit/TestRunner.h>
+#include "config.h"
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #else 
